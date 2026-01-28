a3d8576300c3a06f2988e77ac5b0ca9008c7b415
davberamo <dberlinches2003@gmail.com>
2026-01-28 17:13:04 +0100

    Mejorada la funcion loadNewWord

--- app/src/main/java/com/david/vocabmaster/model/HomeViewModel.kt
+++ app/src/main/java/com/david/vocabmaster/model/HomeViewModel.kt
@@ -1,15 +1,21 @@
 package com.david.vocabmaster.model
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.david.vocabmaster.data.repository.WordRepository
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class HomeViewModel : ViewModel() {
 
     private val repository = WordRepository()
 
     private val _currentWord = MutableStateFlow<Word?>(null)
+
+    private val _isChanging = MutableStateFlow(false)
+    val isChanging: StateFlow<Boolean> = _isChanging
     val currentWord: StateFlow<Word?> = _currentWord
 
     init {
@@ -17,6 +23,13 @@ class HomeViewModel : ViewModel() {
     }
 
     fun loadNewWord() {
-        _currentWord.value = repository.getRandomWord()
+        _isChanging.value = true
+
+        // Simular animación o carga
+        viewModelScope.launch {
+            delay(200)
+            _currentWord.value = repository.getRandomWord()
+            _isChanging.value = false
+        }
     }
 }
