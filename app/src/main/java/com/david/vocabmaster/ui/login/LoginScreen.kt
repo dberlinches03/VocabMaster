4347cbded770c5a267e2e24ed6fed99b53457f0e
davberamo <dberlinches2003@gmail.com>
2026-01-28 16:46:34 +0100

    Declaradas variables de estado en LoginScreen.kt

--- app/src/main/java/com/david/vocabmaster/ui/login/LoginScreen.kt
+++ app/src/main/java/com/david/vocabmaster/ui/login/LoginScreen.kt
@@ -2,8 +2,15 @@ package com.david.vocabmaster.ui.login
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.navigation.NavController
+import com.david.vocabmaster.model.LoginViewModel
 
 @Composable
-fun LoginScreen(navController: NavController)
-{ Text("Pantalla de Login") }
+fun LoginScreen(navController: NavController, viewModel: LoginViewModel = LoginViewModel()) {
+
+    val email by viewModel.email.collectAsState()
+    val password by viewModel.password.collectAsState()
+    val error by viewModel.error.collectAsState()
+}
