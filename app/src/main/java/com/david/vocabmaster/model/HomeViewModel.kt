e61c6f14491aa14fe6da440be05b5905669a034b
davberamo <dberlinches2003@gmail.com>
2026-01-28 17:04:55 +0100

    Añadido viewModel para la pantalla principal

--- app/src/main/java/com/david/vocabmaster/model/HomeViewModel.kt
+++ app/src/main/java/com/david/vocabmaster/model/HomeViewModel.kt
@@ -1,2 +1,22 @@
 package com.david.vocabmaster.model
 
+import androidx.lifecycle.ViewModel
+import com.david.vocabmaster.data.repository.WordRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class HomeViewModel : ViewModel() {
+
+    private val repository = WordRepository()
+
+    private val _currentWord = MutableStateFlow<Word?>(null)
+    val currentWord: StateFlow<Word?> = _currentWord
+
+    init {
+        loadNewWord()
+    }
+
+    fun loadNewWord() {
+        _currentWord.value = repository.getRandomWord()
+    }
+}
