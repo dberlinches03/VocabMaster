a03c7574a2d9247311dc6b85882a82c9b1082da0
davberamo <dberlinches2003@gmail.com>
2026-01-28 18:59:56 +0100

    Implementada la interfaz del DAO LearnedWordDao.kt

--- app/src/main/java/com/david/vocabmaster/data/room/LearnedWordDao.kt
+++ app/src/main/java/com/david/vocabmaster/data/room/LearnedWordDao.kt
@@ -1,5 +1,21 @@
 package com.david.vocabmaster.data.room
 
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
 interface LearnedWordDao {
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(word: LearnedWord)
+
+    @Query("SELECT * FROM learned_words ORDER BY dateLearned DESC")
+    fun getAll(): Flow<List<LearnedWord>>
+
+    @Query("SELECT COUNT(*) FROM learned_words")
+    fun count(): Flow<Int>
+
+    @Query("SELECT * FROM learned_words WHERE word = :word LIMIT 1")
+    suspend fun getbyWord(word: String): LearnedWord?
 }
