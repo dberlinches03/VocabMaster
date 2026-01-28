db371ed0dc798cb204fd1c9f7dc23c26b6b2706e
davberamo <dberlinches2003@gmail.com>
2026-01-28 17:21:54 +0100

    Implementada las preferencias en UserPreferences.kt

--- app/src/main/java/com/david/vocabmaster/data/datastore/UserPreferences.kt
+++ app/src/main/java/com/david/vocabmaster/data/datastore/UserPreferences.kt
@@ -1,2 +1,36 @@
 package com.david.vocabmaster.data.datastore
 
+import android.content.Context
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+val Context.dataStore by preferencesDataStore("user_preferences")
+
+class UserPreferences(private val context: Context) {
+
+    companion object {
+        val DARK_MODE = booleanPreferencesKey("dark_mode")
+        val LANGUAGE = stringPreferencesKey("language")
+    }
+    val darkModeFlow: Flow<Boolean> = context.dataStore.data
+        .map { prefs -> prefs[DARK_MODE] ?: false  }
+
+    val languageFlow: Flow<String> = context.dataStore.data
+        .map { prefs -> prefs[LANGUAGE] ?: "en" }
+
+    suspend fun setDarkMode(enabled: Boolean) {
+        context.dataStore.edit { prefs ->
+            prefs[DARK_MODE] = enabled
+        }
+    }
+
+    suspend fun setLanguage(language: String) {
+        context.dataStore.edit { prefs ->
+            prefs[LANGUAGE] = language
+        }
+    }
+ }
