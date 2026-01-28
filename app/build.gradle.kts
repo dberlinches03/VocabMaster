173b5beb438c31ea0df9e5d35126c3a66429849d
davberamo <dberlinches2003@gmail.com>
2026-01-28 19:05:33 +0100

    Creada la base de datos vocabmaster_db

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.compose.material3)
     implementation(libs.androidx.room.common.jvm)
+    implementation(libs.androidx.room.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
