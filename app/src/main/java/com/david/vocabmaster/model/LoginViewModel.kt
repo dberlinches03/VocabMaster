3209112cf985700c3b39c8ad85dc06b97dc3739d
davberamo <dberlinches2003@gmail.com>
2026-01-28 16:40:31 +0100

    Declaradas las variables de LoginViewModel.kt

--- app/src/main/java/com/david/vocabmaster/model/LoginViewModel.kt
+++ app/src/main/java/com/david/vocabmaster/model/LoginViewModel.kt
@@ -1,2 +1,13 @@
 package com.david.vocabmaster.model
 
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class LoginViewModel : ViewModel() {
+    private val _email = MutableStateFlow("")
+    val email: StateFlow<String> = _email
+
+    private val _password = MutableStateFlow("")
+    val password: StateFlow<String> = _password
+}
