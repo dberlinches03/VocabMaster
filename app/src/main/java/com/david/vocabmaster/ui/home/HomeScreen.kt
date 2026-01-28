6f47e94cc534378cfa4d8f8a2400baefdfba0b72
davberamo <dberlinches2003@gmail.com>
2026-01-28 17:10:04 +0100

    implementada la pantalla de inicio en HomeScreen.kt

--- app/src/main/java/com/david/vocabmaster/ui/home/HomeScreen.kt
+++ app/src/main/java/com/david/vocabmaster/ui/home/HomeScreen.kt
@@ -1,13 +1,48 @@
 package com.david.vocabmaster.ui.home
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.david.vocabmaster.AppScaffold
+import com.david.vocabmaster.model.HomeViewModel
 
 @Composable
-fun HomeScreen(navController: NavController) {
+fun HomeScreen(navController: NavController, viewModel: HomeViewModel = HomeViewModel()) {
+    val word = viewModel.currentWord.collectAsState().value
     AppScaffold(navController) {
-        Text("")
+        if (word == null) {
+            CircularProgressIndicator()
+            return@AppScaffold
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(24.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                word.word,
+                style = MaterialTheme.typography.headlineLarge
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(onClick = {viewModel.loadNewWord()}) {
+                Text("Nueva palabra")
+            }
+        }
     }
 }
