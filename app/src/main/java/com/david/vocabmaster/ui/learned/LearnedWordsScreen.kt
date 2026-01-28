61ba24264a28000ac9dd6803fc191c84392caa46
davberamo <dberlinches2003@gmail.com>
2026-01-28 19:29:35 +0100

    Implementada la pantalla de las palabras aprendidas

--- app/src/main/java/com/david/vocabmaster/ui/learned/LearnedWordsScreen.kt
+++ app/src/main/java/com/david/vocabmaster/ui/learned/LearnedWordsScreen.kt
@@ -1,13 +1,48 @@
 package com.david.vocabmaster.ui.learned
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.david.vocabmaster.AppScaffold
 
 @Composable
 fun LearnedWordsScreen(navController: NavController) {
+
+    val viewModel: LearnedWordsViewModel = viewModel()
+    val words by viewModel.words.collectAsState()
+
     AppScaffold(navController) {
-        Text("Palabras aprendidas")
+
+        Column (modifier = Modifier.padding(16.dp)){
+
+            Text("Palabras aprendidas")
+            Spacer(Modifier.height(16.dp))
+
+            LazyColumn {
+                items(words) { item ->
+                    Card (
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 6.dp)
+                    ){
+                        Column(modifier = Modifier.padding(16.dp)) {
+                            Text(item.word, style = MaterialTheme.typography.titleLarge)
+                            Text(item.definition)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
