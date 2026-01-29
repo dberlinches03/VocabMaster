3efa823aa84b0bdffdc6361a79ee79b32ae9a3ba
dberl <dberlinches2003@gmail.com>
2026-01-29 15:15:27 +0100

    Creada la clase StatsViewModel.kt

---
+++ app/src/main/java/com/david/vocabmaster/ui/stats/StatsViewModel.kt
@@ -0,0 +1,8 @@
+package com.david.vocabmaster.ui.stats
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class StatsViewModel(app: Application) : AndroidViewModel(app) {
+
+}
