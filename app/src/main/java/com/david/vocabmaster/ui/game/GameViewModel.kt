28b7e72aeb02b82133b45119b49dccd4d3892870
dberl <dberlinches2003@gmail.com>
2026-01-29 15:36:45 +0100

    Creada la clase GameViewModel.kt

---
+++ app/src/main/java/com/david/vocabmaster/ui/game/GameViewModel.kt
@@ -0,0 +1,8 @@
+package com.david.vocabmaster.ui.game
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class GameViewModel(app: Application) : AndroidViewModel(app) {
+
+}
